239bb13ae23d77d94981fb622f0a78a9f7c23e96
Hamilton Guirrugo <guirrugohamiltonantonio@gmail.com>
2025-01-23 12:10:20 -0800

    Project Created

---
+++ src/test/java/com/wisemind8h/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisemind8h.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
